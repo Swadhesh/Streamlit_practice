233ce68c64dc53d8bfbdfb71e213cb835158bc35
Swadhesh P U <75163426+Swadhesh@users.noreply.github.com>
2023-07-25 19:43:25 +0530

    Update CgpaCalc.py

--- CgpaCalc.py
+++ CgpaCalc.py
@@ -10,10 +10,10 @@ def calculate_gpa(cgpa, credits, curr_gpa, curr_credits):
 def main():
     st.markdown('<h1 style="color:lightgreen; text-align:center;">Current CGPA calculator</h1>',unsafe_allow_html=True)
     st.markdown('<h3 style="color:darkgreen; text-align:center;">By Swadhesh 20IT108</h3>',unsafe_allow_html=True)
-    cgpa = st.number_input('Enter CGPA till your previous semester:', min_value=0.1, step=0.1, value=0.0)
-    credits = st.number_input('Enter the total credits earned till your previous semester:', min_value=1, step=1, value=0)
-    curr_gpa = st.number_input('Enter GPA of your current semester:', min_value=0.1, step=0.1, value=0.0)
-    curr_credits = st.number_input('Enter credits earned in the current semester:', min_value=1, step=1, value=0)
+    cgpa = st.number_input('Enter CGPA till your previous semester:', min_value=0.1, step=0.1, value=0.1)
+    credits = st.number_input('Enter the total credits earned till your previous semester:', min_value=1, step=1, value=1)
+    curr_gpa = st.number_input('Enter GPA of your current semester:', min_value=0.1, step=0.1, value=0.1)
+    curr_credits = st.number_input('Enter credits earned in the current semester:', min_value=1, step=1, value=1)
 
     if st.button('Calculate current CGPA'):
         gpa = calculate_gpa(cgpa, credits, curr_gpa, curr_credits)
